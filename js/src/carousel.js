c733a6acb336d70c546cef385e0a43a56bf4db2f
GeoSot <geo.sotis@gmail.com>
2021-09-10 02:54:14 +0300

    Carousel: interval is an integer, treat it as such

--- js/src/carousel.js
+++ js/src/carousel.js
@@ -489,7 +489,7 @@ class Carousel extends BaseComponent {
     const slideIndex = Manipulator.getDataAttribute(this, 'slide-to')
 
     if (slideIndex) {
-      config.interval = false
+      config.interval = 0
     }
 
     Carousel.carouselInterface(target, config)
